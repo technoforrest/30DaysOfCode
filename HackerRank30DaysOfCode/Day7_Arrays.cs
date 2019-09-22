ed214fa08148e987751144af02d795916d2135bd
technoforrest <31766195+technoforrest@users.noreply.github.com>
2019-09-22 14:25:11 -0700

    Add files via upload

---
+++ HackerRank30DaysOfCode/Day7_Arrays.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+class Solution
+{
+
+
+
+    static void Main(string[] args)
+    {
+        //Size of array
+        int n = Convert.ToInt32(Console.ReadLine());
+
+        //fill the array
+        int[] arr = Array.ConvertAll(Console.ReadLine().Split(' '), arrTemp => Convert.ToInt32(arrTemp));
+
+        //reverse the array
+        Array.Reverse(arr);
+
+        //print the array
+        for (int i = 0; i < n; i++)
+        {
+            Console.Write(arr[i] + " ");
+        }
+    }
+}
+
