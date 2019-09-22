bbea595a4760cb10c83fe11b0df3047eaf37634d
technoforrest <31766195+technoforrest@users.noreply.github.com>
2019-09-21 22:19:44 -0700

    Add files via upload

---
+++ HackerRank30DaysOfCode/Day6_Review.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+class Solution
+{
+    static void Main(String[] args)
+    {
+        int x;
+        x = Convert.ToInt32(Console.ReadLine());
+        //x is the number of test cases.
+        for (int i = 0; i < x; i++)
+        {
+            string s;
+            string s1 = "";
+            string s2 = "";
+            s = Console.ReadLine();
+
+            for (int j = 0; j < s.Length; j++)
+            {
+                //if index is even
+                if (j % 2 == 0)
+                {
+                    //new string with char from even indexed
+                    s1 += s[j];
+                }
+                //else it is odd
+                else
+                {
+                    //new string with char from odd indexed
+                    s2 += s[j];
+                }
+            }
+            //concatenate for new string
+            Console.WriteLine(s1 + " " + s2);
+        }
+
+    }
+}
+
